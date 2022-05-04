343fe084f2cc47f754678538759627d98db011c6
unknown <leonardoxavier092@gmail.com>
2022-05-04 19:22:32 -0300

    feat: new button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,10 @@
+import { FiSearch } from "react-icons/fi";
+import {ButtonSearch} from "./styles"
+
+const Button = (props) => (
+  <ButtonSearch onClick={props.function}>
+    <FiSearch size={25} color="#000" />
+  </ButtonSearch>
+);
+
+export default Button;
